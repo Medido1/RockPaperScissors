22defaf73737612c9a060d1e81e87a4d51ee678b
Medido1 <medi1222@gmail.com>
2023-01-30 12:14:40 +0100

    add UI to play one round
    change previous function of one round.
    add clickable choices of rock paper and sicssor
    add div to show result of each round which change its background color depending on result

--- index.js
+++ index.js
@@ -1,94 +1,72 @@
+let roundWinner = ""; let playerScore=0; let ComputerScore = 0;
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+const round_result = document.getElementById("round_result");
+
+rock.addEventListener("click", ()=> {
+	playRound("rock")
+})
+paper.addEventListener("click", ()=> {
+	playRound("paper")
+})
+scissor.addEventListener("click", ()=> {
+	playRound("scissor")
+})
+
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    let choice = "";
-    switch (randomNumber){
-        case 1:
-            choice = "rock";
-            break;
-        case 2:
-            choice = "paper";
-            break;
-        case 3:
-            choice = "scissor";
-            break;
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let choice = "";
+  switch (randomNumber){
+    case 1:
+      choice = "rock";
+      break;
+    case 2:
+      choice = "paper";
+      break;
+    case 3:
+      choice = "scissor";
+      break;
     }
     return choice;
 }
 
-function gameRound(computerChoice, playerChoice){
-    let rock = /rock/i; let paper = /paper/i; let scissor = /scissor/i
-    let result; let winner; let loser;
-    if (computerChoice === "rock" && playerChoice.match(rock)){
-        result = "draw!!";
-    }
-    else if (computerChoice === "rock" && playerChoice.match(paper)){
-        result = "You win!!";
-    }
-    else if (computerChoice === "rock" && playerChoice.match(scissor)){
-        result = "You lose!!";
-    }
-    else if (computerChoice === "paper" && playerChoice.match(rock)){
-        result = "You lose!!";
-    }
-    else if (computerChoice === "paper" && playerChoice.match(paper)){
-        result = "draw!!"
-    }
-    else if (computerChoice === "paper" && playerChoice.match(scissor)){
-        result = "You win!!";
-    }
-    else if (computerChoice === "scissor" && playerChoice.match(rock)){
-        result = "You win!!";
-    }
-    else if (computerChoice === "scissor" && playerChoice.match(paper)){
-        result = "You lose!!";
-    }
-    else if (computerChoice === "scissor" && playerChoice.match(scissor)){
-        result = "draw!!"
-    }
-    else {
-        return (alert("Invalid input"), computerChoice = getComputerChoice(),
-        playerChoice = prompt("Rock, Paper or Scissor?"), gameRound(computerChoice, playerChoice));
-    }
-
-    if (result === "You win!!"){
-        winner = playerChoice;
-        loser = computerChoice;
-    }
-    else if (result === "You lose!!"){
-        winner = computerChoice;
-        loser = playerChoice;
-    }
-    else if (result === "draw!!"){
-        return ("draw!!");
-    }
-    
-    return (`${result}  ${winner} beats ${loser}`);
+function playRound(playerChoice, computerChoice){
+	computerChoice = getComputerChoice();
+  if (playerChoice === computerChoice){
+    roundWinner = "draw";
+  }
+  else if(
+		(playerChoice === "rock" && computerChoice === "scissor") ||
+    (playerChoice === "paper" && computerChoice === "rock")||
+    (playerChoice === "scissor" && computerChoice === "paper")){
+      roundWinner = playerChoice;
+      playerScore++
+  }
+	else if(
+		(playerChoice === "rock" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "scissor")||
+    (playerChoice === "scissor" && computerChoice === "rock")){
+      roundWinner = computerChoice;
+      ComputerScore++
+  }
+	showRoundResult(roundWinner, playerChoice, computerChoice)
 }
 
-function game(){
-    let computerChoice; let playerChoice; let result;
-    let playerScore = 0; let ComputerScore = 0;
-    for (let i = 0; i < 5; i++){
-        computerChoice = getComputerChoice();
-        playerChoice = prompt("Rock, Paper or Scissor?");
-        result =gameRound(computerChoice, playerChoice);
-        alert(result);
-        if (result.match("You win!!")){
-            playerScore += 1;
-        }
-        else if (result.match("You lose!!")){
-            ComputerScore += 1;
-        }
-    }
-    if (playerScore > ComputerScore || playerScore === 3){
-        return (`You win!! You:${playerScore} Computer:${ComputerScore}`);
-    }
-    else if (playerScore < ComputerScore || ComputerScore === 3){
-        return (`You lose!! You:${playerScore} Computer:${ComputerScore}`);
-    }
-    else {
-        return ("Draw!!")
-    }
+function showRoundResult(roundWinner, playerChoice, computerChoice){
+	if (roundWinner === "draw"){
+		round_result.textContent = "it'a Draw!!";
+		round_box.style.backgroundColor = "green";
+	}
+	else if (roundWinner === playerChoice){
+		round_result.textContent = `You win!! ${playerChoice} beats ${computerChoice}`;
+		round_box.style.backgroundColor = "blue";
+	}
+	else if (roundWinner === computerChoice){
+		round_result.textContent = `You lose :( ${computerChoice} beats ${playerChoice}`;
+		round_box.style.backgroundColor = "red";
+	}
 }
 
 
